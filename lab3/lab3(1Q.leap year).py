af0002f370dd3ccbd44d11a71f6bcb2a7a6fe19e
mounika2002-aji <ajemeramounika@gmail.com>
2025-01-02 20:00:11 +0530

    Add files via upload

---
+++ lab3/lab3(1Q.leap year).py
@@ -0,0 +1,6 @@
+#1Q.Take input from the user
+year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
